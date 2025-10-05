1e0086409e0d59568257d7c3336006aa92433a22
makano <makanobush@gmail.com>
2025-10-05 22:02:11 +0300

    experimental macros

---
+++ _macros.lua
@@ -0,0 +1,14 @@
+return [[
+macro {
+  for_each ($key, $iterator, $expr) {
+    for $key in ipairs($iterator) do
+      $expr
+    end
+  }
+}
+macro {
+  hello ($something) {
+    print("Hello, " .. $something)
+  }
+}
+]]
